08a8dfb16398e97ffb703d9dfc28e28f8a30f450
Liam O'Boyle <liam@elyobo.net>
2017-12-08 12:41:09 +1100

    Add stub /api/create handler function and mapping.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const { json } = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const api = () => {
+  const router = express.Router()
+
+  router.use(json());
+
+  router.post('/create', (req, res) => {
+    res.set('Content-Type', 'application/json; charset=utf-8');
+    res.send(JSON.stringify(req.body, null, '  '));
+  });
+
+  return router;
+};
+
+const app = express();
+app.use('/api', api());
+
+exports.app = functions.https.onRequest(app);
