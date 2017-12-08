d091611c77302ad645c5ec5611ed9da1af1456bf
Andrew Carr <andrew@2metres.com>
2017-12-08 15:55:47 +1100

    Import schema from JSON

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require('firebase-functions');
 const express = require('express');
 const { json } = require('body-parser');
 
+const schema = require('./data/schema.json');
+
 const api = () => {
   const router = express.Router()
 
@@ -10,70 +12,7 @@ const api = () => {
   router.post('/create', (req, res) => {
     res.set('Content-Type', 'application/json; charset=utf-8');
     res.send(
-      JSON.stringify(
-        {
-          "title": "Template Options",
-          "type": "object",
-          "properties": {
-            "Suburb": {
-              "type": "string",
-              "enum": [
-                "Sydney 2000",
-                "Melbourne 3000",
-                "Brisbane 4000"
-              ]
-            }
-          },
-          "required": [
-            "Suburb"
-          ],
-          "dependencies": {
-            "Suburb": {
-              "oneOf": [
-                {
-                  "properties": {
-                    "Suburb": {
-                      "enum": [ "Melbourne 3000" ]
-                    },
-                    "Select an Agent": {
-                      "type": "number",
-                      "enum": [3001, 3002],
-                      "enumNames": [ "Liam O’Boyle", "Andrew Carr" ]
-                    }
-                  },
-                  "required": [ "Suburb", "Select an Agent" ]
-                },
-                {
-                  "properties": {
-                    "Suburb": {
-                      "enum": [ "Sydney 2000" ]
-                    },
-                    "Select an Agent": {
-                      "type": "number",
-                      "enum": [2001, 2002],
-                      "enumNames": [ "Matt Gay", "Adrian Gimenz" ]
-                    }
-                  },
-                  "required": [ "Suburb", "Select an Agent" ]
-                },
-                {
-                  "properties": {
-                    "Suburb": {
-                      "enum": [ "Brisbane 4000" ]
-                    },
-                    "Select an Agent": {
-                      "type": "number",
-                      "enum": [4001],
-                      "enumNames": [ "Jordan Tormey" ]
-                    }
-                  },
-                  "required": [ "Suburb", "Select an Agent" ]
-                }
-              ]
-            }
-          }
-        }
-      )
+      JSON.stringify(schema)
     );
   });
 
