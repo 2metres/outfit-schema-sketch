ba9c6c7a5714716a3103eee32765e35066351db7
Liam O'Boyle <liam@elyobo.net>
2017-12-08 18:10:42 +1100

    Better devserver outout.
    Adds prefix on newlines too, drops redundant extra proxy settings.

--- devserver.js
+++ devserver.js
@@ -6,11 +6,22 @@ const { spawn } = require('child_process');
 const RESET = '\x1b[0m';
 const YELLOW = '\x1b[33m';
 const BLUE = '\x1b[34m';
+const RED = '\x1b[31m';
+const MAGENTA = '\x1b[35m';
+const UNDERSCORE = '\x1b[4m';
 
+const reformat = (colour, name) => data => {
+  const prefix = `${colour}${name}${RESET} `;
+  const _data = String(data)
+    .replace(/\n$/, '')
+    .replace(/^\n+/m, '')
+    .replace(/\n/mg, `\n${prefix}`);
+  return prefix + _data + '\n';
+};
 
-const wrap = (name, color, ex, args) => {
+const wrap = (name, colour, ex, args) => {
   const child = spawn(ex, args);
-  const out = data => `${color}${name}>${RESET} ${data}`;
+  const out = reformat(colour, name);
   child.stdout.on('data', data => process.stdout.write(out(data)));
   child.stderr.on('data', data => process.stderr.write(out(data)));
   return child;
@@ -23,17 +34,15 @@ const app = express();
 
 app.use('/api', proxy('localhost:5000', {
   proxyReqPathResolver: req => `/api${req.url}`,
-  parseReqBody: true,
-  preserveHostHdr: true,
 }));
 app.use('/', proxy('localhost:5010'));
 
 app.locals.port = process.env.PORT ? +process.env.PORT : 5020;
 const server = app
   .listen(app.locals.port, function debugPort() {
-    console.log(`Server running on port http://localhost:${app.locals.port}`);
+   process.stdout.write(`Server running on port ${UNDERSCORE}${RED}http://localhost:${app.locals.port}${RESET}\n\n`);
   })
   .on('error', error => {
-    console.error(`Failed to start server: ${error.message}`);
+    process.stderr.write(`Failed to start server: ${error.message}\n`);
     server.close();
   });
