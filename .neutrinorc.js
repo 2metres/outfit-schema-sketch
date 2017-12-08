2b1ff4d3763368a4e083d01471611c3ec05ad8a0
Liam O'Boyle <liam@elyobo.net>
2017-12-08 16:01:29 +1100

    Add a dev server.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,5 +1,10 @@
 module.exports = {
   options: {
     output: 'public',
-  }
+  },
+  use: [
+    ['neutrino-preset-react', {
+      devServer: { port: 5010 }
+    }]
+  ],
 };
---
+++ devserver.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const express = require('express');
+const proxy = require('express-http-proxy');
+const { spawn } = require('child_process');
+
+const RESET = '\x1b[0m';
+const YELLOW = '\x1b[33m';
+const BLUE = '\x1b[34m';
+
+
+const wrap = (name, color, ex, args) => {
+  const child = spawn(ex, args);
+  const out = data => `${color}${name}>${RESET} ${data}`;
+  child.stdout.on('data', data => process.stdout.write(out(data)));
+  child.stderr.on('data', data => process.stderr.write(out(data)));
+  return child;
+};
+
+const functions = wrap('functions', BLUE, 'npm', ['run', 'serve']);
+const neutrino  = wrap('neutrino', YELLOW, 'npm', ['run', 'start']);
+
+const app = express();
+
+app.use('/api', proxy('localhost:5000', {
+  proxyReqPathResolver: req => `/api${req.url}`,
+  parseReqBody: true,
+  preserveHostHdr: true,
+}));
+app.use('/', proxy('localhost:5010'));
+
+app.locals.port = process.env.PORT ? +process.env.PORT : 5020;
+const server = app
+  .listen(app.locals.port, function debugPort() {
+    console.log(`Server running on port http://localhost:${app.locals.port}`);
+  })
+  .on('error', error => {
+    console.error(`Failed to start server: ${error.message}`);
+    server.close();
+  });
